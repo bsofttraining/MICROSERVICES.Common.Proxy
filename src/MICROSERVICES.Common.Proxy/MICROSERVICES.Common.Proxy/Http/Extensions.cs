ae0db6212e2a64f95f48b6ea9a2c3e9cf6759a66
ictismart <ivan.cuadros@tismart.com>
2019-11-27 16:39:13 -0500

    Primera version del Proxy

---
+++ src/MICROSERVICES.Common.Proxy/MICROSERVICES.Common.Proxy/Http/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MICROSERVICES.Common.Proxy.Http
+{
+    public static class Extensions
+    {
+        private static readonly string ProxySectionName = "proxy";
+        public static IServiceCollection AddProxyHttp(this IServiceCollection services)
+        {
+            IConfiguration configuration;
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                configuration = serviceProvider.GetService<IConfiguration>();
+            }
+
+            var options = configuration.GetOptions<ProxyOptions>(ProxySectionName);
+
+            if (options.Enabled)
+            {
+                services.AddSingleton<IHttpClient, CustomHttpClient>();
+            }
+
+            return services;
+        }
+
+
+    }
+}
---
+++ src/MICROSERVICES.Common.Proxy/MICROSERVICES.Common.Proxy/Http/ProxyOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MICROSERVICES.Common.Proxy.Http
+{
+    public class ProxyOptions
+    {
+        public bool Enabled { get; set; }
+    }
+}
